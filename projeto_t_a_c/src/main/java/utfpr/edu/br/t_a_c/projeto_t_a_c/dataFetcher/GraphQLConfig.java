7f3657453a7ca137b0c265dfce761dff5887ea6e
Jian Licio de Oliveira <jian_oliveira12@hotmail.com>
2024-06-13 20:00:32 -0300

    testando sonar

--- projeto_t_a_c/src/main/java/utfpr/edu/br/t_a_c/projeto_t_a_c/dataFetcher/GraphQLConfig.java
+++ projeto_t_a_c/src/main/java/utfpr/edu/br/t_a_c/projeto_t_a_c/dataFetcher/GraphQLConfig.java
@@ -9,27 +9,16 @@ import graphql.schema.GraphQLSchema;
 @Configuration
 public class GraphQLConfig {
 
-    // @Bean
-    // public GraphQLSchema graphQLSchema(PessoaDataFetcher pessoaDataFetcher) {
-    // return SchemaParser.newParser()
-    // .files("graphql/schema.graphqls")
-    // .resolvers(pessoaDataFetcher)
-    // .build()
-    // .makeExecutableSchema();
-    // }
-
     @Bean
     public GraphQLSchema graphQLSchema(PessoaDataFetcher pessoaDataFetcher) {
         try {
-            // Cria e retorna o esquema GraphQL baseado no arquivo de schema e no data
-            // fetcher
+
             return SchemaParser.newParser()
                     .files("graphql/schema.graphqls") // Certifique-se de que este caminho está correto
                     .resolvers(pessoaDataFetcher) // Adiciona o data fetcher
                     .build()
                     .makeExecutableSchema();
         } catch (Exception e) {
-            // Loga e lida com exceções de forma apropriada
             System.err.println("Erro ao criar o schema GraphQL: " + e.getMessage());
             throw new RuntimeException("Erro ao criar o schema GraphQL", e);
         }
