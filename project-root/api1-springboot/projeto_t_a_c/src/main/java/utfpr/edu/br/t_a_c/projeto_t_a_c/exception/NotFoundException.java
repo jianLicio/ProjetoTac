198385604f6d24c56dc3a9bf38a0ddc85571c312
Jian Licio de Oliveira <jian_oliveira12@hotmail.com>
2024-06-24 20:32:09 -0300

    separando repositórios
    a partir de agora, cada subprojeto tem seu repositório

--- project-root/api1-springboot/projeto_t_a_c/src/main/java/utfpr/edu/br/t_a_c/projeto_t_a_c/exception/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package utfpr.edu.br.t_a_c.projeto_t_a_c.exception;
-
-public class NotFoundException extends Exception{
- public NotFoundException(String message) {
-        super(message);
-    }
-}
--- project-root/api1-springboot/projeto_t_a_c/src/main/java/utfpr/edu/br/t_a_c/projeto_t_a_c/repository/DispositivoRepository.java
+++
@@ -1,9 +0,0 @@
-package utfpr.edu.br.t_a_c.projeto_t_a_c.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import utfpr.edu.br.t_a_c.projeto_t_a_c.model.Dispositivo;
-
-public interface DispositivoRepository  extends JpaRepository<Dispositivo, Long>{
-
-}
